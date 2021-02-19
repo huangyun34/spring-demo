fda05b3a626e046a2a0d709e9984f72e785660bd
黄云 <huangyun34@163.com>
2021-02-19 16:17:31 +0800

    hello world

--- src/main/java/com/my/spring/controller/TestController.java
+++ src/main/java/com/my/spring/controller/TestController.java
@@ -1,13 +1,14 @@
 package com.my.spring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.DefaultParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 
 @RestController
 @RequestMapping("/test")
@@ -19,6 +20,15 @@ public class TestController {
         ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = requestAttributes.getRequest();
         System.out.println(request.getParameter("name"));
+        //通过流的方式获取参数名称，spring封装的工具类
+        ParameterNameDiscoverer parameterNameDiscoverer = new DefaultParameterNameDiscoverer();
+        Method method = ReflectionUtils.findMethod(TestController.class, "query", String.class);
+        String[] parameterNames = parameterNameDiscoverer.getParameterNames(method);
+        if (parameterNames.length > 0) {
+            for (String parameterName : parameterNames) {
+                System.out.println(parameterName);
+            }
+        }
         return name;
     }
 }
